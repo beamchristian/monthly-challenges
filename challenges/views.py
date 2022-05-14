2c42a6b541e34d6b1b6a397012cf5c4f9b686065
beamchristianE <ckbeamcoding@gmail.comm>
2022-05-14 09:41:34 -0400

    first view

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,9 @@
+from urllib import response
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("This works!")
