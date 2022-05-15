abea2f0640466ac9ccf0a9a9397a85f8c22b16f5
beamchristianE <ckbeamcoding@gmail.comm>
2022-05-15 08:43:20 -0400

    dynamic monthly challenges view rendering

--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,20 @@
 from urllib import response
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == 'february':
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == 'march':
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
