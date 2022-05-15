8c34680e2b3b365fdd4c72b882dabc5d4cc37772
beamchristianE <ckbeamcoding@gmail.comm>
2022-05-15 08:53:59 -0400

    conditional rendering of url through array with try and except

--- challenges/views.py
+++ challenges/views.py
@@ -2,19 +2,30 @@ from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "may": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "june": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "july": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "august": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "september": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "october": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "november": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim.",
+    "december": "Qui occaecat mollit reprehenderit adipisicing incididunt eiusmod mollit reprehenderit ex minim."
+}
+
 # Create your views here.
 
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = "Eat no meat for the entire month!"
-    elif month == 'february':
-        challenge_text = "Walk for at least 20 minutes every day!"
-    elif month == 'march':
-        challenge_text = "Learn Django for at least 20 minutes every day!"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    
